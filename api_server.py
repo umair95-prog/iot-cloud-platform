d14dce93c94ca1f79926bdd8a12a6c80a36e69d8
Muhammad Umair Shakir <umairrao95@gmail.com>
2026-05-16 13:53:37 +0200

    Add Prometheus and Grafana monitoring stack

--- api_server.py
+++ api_server.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import datetime
 from fastapi.responses import HTMLResponse
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 
 app = FastAPI()
 
@@ -10,6 +12,11 @@ latest_data = {
     "temperature": None,
     "timestamp": None
 }
+
+temperature_gauge = Gauge(
+    "iot_temperature_celsius",
+    "Latest IoT sensor temperature in Celsius"
+)
 
 # Data model
 class SensorData(BaseModel):
@@ -22,6 +29,8 @@ def receive_sensor_data(data: SensorData):
     latest_data["temperature"] = data.temperature
     latest_data["timestamp"] = datetime.now().isoformat()
 
+    temperature_gauge.set(data.temperature)
+
     return {
         "message": "Sensor data received",
         "data": latest_data
@@ -31,6 +40,11 @@ def receive_sensor_data(data: SensorData):
 @app.get("/sensor-data")
 def get_sensor_data():
     return latest_data
+
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 @app.get("/", response_class=HTMLResponse)
 def dashboard():
